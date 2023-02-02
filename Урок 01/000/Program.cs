091933a356ce85b5e474f9a83c2949d685cb1130
Rakhman <binataliev-a-g@yandex.ru>
2023-02-02 11:12:13 +0300

    Lesson1 0.1

---
+++ Урок 01/000/Program.cs
@@ -0,0 +1,15 @@
+﻿// C Клавиатуры вводится два числа. Найти и вывести на экран их сумму
+
+//string s;
+int a,b,sum;
+System.Console.Write("Введите а:");
+a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите b:");
+b=Convert.ToInt32(Console.ReadLine());
+sum=a+b;
+//1. Конкатенация (склевание) строк
+System.Console.WriteLine(a+"+"+b+"="+Convert.ToString(sum));
+//2. Форматированный вывод
+System.Console.WriteLine("{0:F2}+{1:E5}={2:G4}",a,b,sum);
+//3. Интерполяция строк
+System.Console.WriteLine($"{a}+{b}={sum}");
---
+++ Урок 01/003/Program.cs
@@ -0,0 +1,16 @@
+﻿// С клавиатуры вводятся два числа a и b. Найти максимальное из них.
+
+int a,b;
+System.Console.Write("Введите а:");;
+a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите b:");;
+b=Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    System.Console.Write($"Максимальное число:{a}");
+}
+else
+{
+    System.Console.Write($"Максимальное число:{b}");
+}
